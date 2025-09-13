f20ad57c0d1428d91cb403219444cefb823b9ded
Mario Basanta Marchan <mario.basantamarchan@colorado.edu>
2025-09-13 16:38:03 +0800

    chore: assignment4-part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,9 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+BIN_DIR="/bin"
+APP_CONF_DIR="/etc/finder-app/conf"
+username=$(cat "$APP_CONF_DIR/username.txt")
 
 if [ $# -lt 3 ]
 then
@@ -32,11 +34,16 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat ../conf/assignment.txt)
+# update reference of assignment
+assignment=$(cat "$APP_CONF_DIR/assignment.txt")
 
-if [ "$assignment" != 'assignment1' ]
+if [ $assignment != 'assignment1' ]
 then
 	mkdir -p "$WRITEDIR"
+
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+	#The quotes signify that the entire string in WRITEDIR is a single string.
+	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
 	then
 		echo "$WRITEDIR created"
@@ -44,27 +51,26 @@ then
 		exit 1
 	fi
 fi
+echo "Removing the old writer utility and compiling as a native application"
+#make clean
+#make
 
-# Run writer.sh for NUMFILES times
-for i in $(seq 1 $NUMFILES)
+for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	"$BIN_DIR/writer" "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-# Run finder.sh
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$("$BIN_DIR/finder.sh" "$WRITEDIR" "$WRITESTR")
 
-# Clean up
+# remove temporary directories
 rm -rf /tmp/aeld-data
 
-# Check result
 set +e
-echo "${OUTPUTSTRING}" | grep "${MATCHSTR}"
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}" > /tmp/assignment4-result.txt
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
 else
-	echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
-
